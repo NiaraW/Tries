7d7aae8c57f0339603f821dbcb28dd5e373c1d78
Ni'ara Williams <2024161688@ub.edu.bz>
2025-11-14 19:37:22 -0600

    Create struct

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+const int ALPHABET_SIZE = 26;
+
+// Trie node
+struct TrieNode {
+    TrieNode* children[ALPHABET_SIZE];
+    bool isEndOfWord;
+
+    TrieNode() {
+        isEndOfWord = false;
+        for (int i = 0; i < ALPHABET_SIZE; i++)
+            children[i] = nullptr;
+    }
+};
