1deb049173b4e59145f9378b3be7612e4561afd4
Ni'ara Williams <2024161688@ub.edu.bz>
2025-11-14 19:46:09 -0600

    Implement Delete

--- main.cpp
+++ main.cpp
@@ -38,3 +38,39 @@ bool search(TrieNode* root, const string &key) {
 
     return curr->isEndOfWord;
 }
+bool isEmpty(TrieNode* node) {
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+        if (node->children[i] != nullptr)
+            return false;
+    return true;
+}
+
+TrieNode* deleteKey(TrieNode* root, const string &key, int depth = 0) {
+    if (!root)
+        return nullptr;
+
+    // If we've reached the end of the word
+    if (depth == key.size()) {
+        if (root->isEndOfWord)
+            root->isEndOfWord = false;
+
+        // If no children → delete node
+        if (isEmpty(root)) {
+            delete root;
+            return nullptr;
+        }
+
+        return root;
+    }
+
+    int index = key[depth] - 'a';
+    root->children[index] = deleteKey(root->children[index], key, depth + 1);
+
+    // If current node is useless
+    if (isEmpty(root) && !root->isEndOfWord) {
+        delete root;
+        return nullptr;
+    }
+
+    return root;
+}
